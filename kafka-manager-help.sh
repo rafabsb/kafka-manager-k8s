33aa7516c00b910f4b3def505218987d392bde03
rafabsb <muller.rafael@gmail.com>
2017-10-23 08:43:19 -0200

    1st commit

---
+++ kafka-manager-help.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Hi,"
+echo ""
+echo "This image is basically just the kafka-manager (yahoo/kafka-manager), and should only be used with kubernetes."
+echo ""
+echo "No need to set an entrypoint, Kubernetes yml files will do the rest"
+echo ""
+echo "Enjoy Kafka Manager!"
+echo ""
